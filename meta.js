76b000f13b94910733389d4a8c0c07275e3c46f9
Jules Bonnard <julesbonnard@gmail.com>
2018-01-29 18:20:30 +0100

    add validation to project name

--- meta.js
+++ meta.js
@@ -20,7 +20,12 @@ module.exports = {
       type: "string",
       required: true,
       message: "Name your project (lowercase and dashes only)",
-      default: "my-awesome-project"
+      default: "my-awesome-project",
+      validate (input) {
+        const regex = /^[a-z0-9-][a-z0-9]+$/
+        if (input.match(regex) !== null) return true
+        return false
+      }
     },
     description: {
       type: "string",
