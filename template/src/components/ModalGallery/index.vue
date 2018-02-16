c90834834418e6c4f677b340b45fb778df752f66
Jacky Fong <jacky.fong@afp.com>
2018-02-16 15:51:19 +0100

    Refactor modal component, add fields (like closeColour) for customization

--- template/src/components/ModalGallery/index.vue
+++ template/src/components/ModalGallery/index.vue
@@ -1,141 +1,77 @@
 <template>
   <main>
-    <h2>{{ $t('headline') }}</h2>
+    <h2>{{ $t('headline') }}</h2> <!-- $t Check /src/locales/en.txt -->
     <h3>{{ $t('subhead') }}</h3>
 
     <div class="gallery">
-      <div v-for="(element, index) in gallery" class="gallery-element" :key="index" v-on:click="openModal(element.image, element.title, element.description, element.source)">
+      <div v-for="(element, index) in gallery" class="gallery-element" :key="index" v-on:click="openModal(element)">
         <img class="thumbnail" :src="getPhoto(element.thumbnail)" alt="">
       </div>
     </div>
 
     <transition name="fade">
-      <div v-if="showModal" class="modal">
-        <div class="modal-mask" v-on:click="closeModal"></div>
-        <div class="modal-container">
-          <div class="close" v-on:click="closeModal">
-            <img src="../../assets/svg/close.svg" alt="close">
-          </div>
-          <img class="modal-image" :src="getPhoto(modalImage)" alt="modal image">
-          <div class="text">
-            <h4 class="title">{{ modalTitle }}</h4>
-            <p class="description">{{ modalDescription }}</p>
-            <p class="source">{{ modalSource }}</p>
-          </div>
-        </div>
-      </div>
+      <modal v-if="showModal" :modal="modal" v-on:closeModal="closeModal"></modal>
     </transition>
   </main>
 </template>
 
 <script>
-  export default {
-    name: 'modal-gallery',
+import Modal from './Modal'
 
-    data () {
-      return {
-        showModal: false,
-        modalDescription: '',
-        modalImage: null,
-        modalTitle: '',
-        modalSource: ''
-      }
-    },
+export default {
+  name: 'modal-gallery',
 
-    computed: {
-      gallery () {
-        return this.$t('gallery')
-      }
-    },
+  components: { Modal },
 
-    methods: {
-      getPhoto (fileName) {
-        return require.context('../../assets/img/modalgallery')('./' + fileName)
-      },
-
-      openModal (image, title, description, source) {
-        this.modalImage = image
-        this.modalDescription = description
-        this.modalTitle = title
-        this.modalSource = source
-
-        this.showModal = true
-      },
-
-      closeModal () {
-        this.showModal = false
+  data () {
+    return {
+      showModal: false,
+      modal: {
+        image: '',
+        caption: '',
+        title: '',
+        subtitle: '',
+        description: '',
+        source: '',
+        closeColour: ''
       }
     }
+  },
+
+  computed: {
+    gallery () {
+      return this.$t('gallery')
+    }
+  },
+
+  methods: {
+    getPhoto (fileName) {
+      return require.context('../../assets/img/modalgallery')('./' + fileName)
+    },
+
+    openModal (element) {
+      this.modal.image = element.image
+      this.modal.caption = element.caption
+      this.modal.image2 = element.image2
+      this.modal.caption2 = element.caption2
+      this.modal.description = element.description
+      this.modal.title = element.title
+      this.modal.subtitle = element.subtitle
+      this.modal.source = element.source
+      this.modal.closeColour = element.closeColour
+
+      this.showModal = true
+    },
+
+    closeModal () {
+      this.showModal = false
+    }
   }
+}
 </script>
 
 <style lang="scss" scoped>
   @import '~@/assets/sass/variables.scss';
-
-  .modal {
-    .modal-mask {
-      height: 100%;
-      width: 100%;
-      background: rgba(0, 0, 0, 0.5);
-      position: absolute;
-      top: 0;
-      left: 0;
-      z-index: 10;
-    }
-
-    .modal-container {
-      position: fixed;
-      z-index: 99;
-      max-width: 580px;
-      width: 100%;
-      background: #FFFFFF;
-      top: 0;
-      left: 50%;
-      transform: translateX(-50%);
-      transition: width 0.2s ease;
-      border-radius: 4px;
-
-      .close {
-        position: absolute;
-        top: 0;
-        right: 0;
-        padding: 15px;
-
-        &>img {
-          width: 20px;
-        }
-
-        &:hover {
-          cursor: pointer;
-        }
-      }
-
-      .modal-image {
-        border-radius: 4px 4px 0 0;
-        width: 100%;
-      }
-
-      .text {
-        padding: 16px 24px 24px;
-
-        .title {
-          font-size: 2rem;
-          text-transform: uppercase;
-        }
-
-        .description {
-          font-size: 1.5rem;
-          margin-top: 20px;
-        }
-
-        .source {
-          font-size: 1.3rem;
-          margin: 0;
-          font-style: italic;
-        }
-      }
-    }
-  }
 
   .gallery {
     display: flex;
@@ -159,6 +95,8 @@
 
       .thumbnail {
         transition: transform 0.2s ease;
+        width: 100%;
+        border-radius: 5px;
       }
 
       &:hover .thumbnail {
@@ -186,6 +124,8 @@
       margin-left: -10px;
 
       .gallery-element {
+        height: 140px;
+        width: 140px;
         margin-left: 20px;
         margin-top: 20px;
       }
