86a25c993b85ca216e684acd7a1f03f0295a71dc
Clement Procureur <clement@sgrx04.afp.local>
2018-04-11 17:35:33 +0200

    merge i18n strategies

--- template/src/i18n/messages.js
+++ template/src/i18n/messages.js
@@ -1,10 +1,10 @@
-import ar from '@/i18n/locales/ar'
-import de from '@/i18n/locales/de'
-import en from '@/i18n/locales/en'
-import es from '@/i18n/locales/es'
-import fr from '@/i18n/locales/fr'
-import it from '@/i18n/locales/it'
-import pt from '@/i18n/locales/pt'
+import ar from '@/i18n/locales/ar.txt'
+import de from '@/i18n/locales/de.txt'
+import en from '@/i18n/locales/en.txt'
+import es from '@/i18n/locales/es.txt'
+import fr from '@/i18n/locales/fr.txt'
+import it from '@/i18n/locales/it.txt'
+import pt from '@/i18n/locales/pt.txt'
 
 const message = { ar, de, en, es, fr, it, pt }
 
