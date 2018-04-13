8c75a004edea47e5f14d8d56fb70f49ec28831b4
Clement Procureur <clement@sgrx04.afp.local>
2018-04-13 17:54:32 +0200

    / route by default

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -21,10 +21,7 @@ Vue.use(Router)
 const router = new Router({
   routes: [
     {
-      path: '/', redirect: { name: 'home' }
-    },
-    {
-      path: '/home',
+      path: '/',
       name: 'home',
       component: Home
     },
