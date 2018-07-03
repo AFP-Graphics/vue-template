32ed8aa10baf2acb6c05b954a356136e864e0819
Jules Bonnard <julesbonnard@gmail.com>
2018-07-03 18:07:53 +0200

    fix analytics

--- template/src/main.js
+++ template/src/main.js
@@ -2,12 +2,38 @@ import Vue from 'vue'
 import i18n from '@/i18n'
 import router from '@/router'
 // import store from '@/store'
-import '@/utils/googleAnalytics'
+import VueAnalytics from 'vue-analytics'
 import Modernizr from 'modernizr'
 
 import App from '@/App'
 
 window.Modernizr = Modernizr
+
+const isProd = process.env.NODE_ENV === 'production'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-64253904-2',
+  router,
+  autoTracking: {
+    exception: true,
+    pageviewOnLoad: true,
+    pageviewTemplate (route) {
+      return {
+        page: `${window.location.pathname}${route.name}`,
+        title: document.title,
+        location: window.location.href
+      }
+    }
+  },
+  set: [
+    { field: 'anonymizeIp', value: true },
+    { field: 'forceSSL', value: true }
+  ],
+  debug: {
+    enabled: !isProd,
+    sendHitTask: isProd
+  }
+})
 
 new Vue({ // eslint-disable-line no-new
   router,
